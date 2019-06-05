cf1b39daf0ac25e353a22c7b8e428f773191f02b
Geoffrey Gaurano <geoffrey.gaurano@gmail.com>
2019-06-05 21:14:31 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,7 @@
 def introduction(name)
   puts "Hi, my name is" + " " + name.to_s + "."
 end
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is" + " " + name.to_s + " " + "and I am learning to program in" + " " + language.to_s + "."
+end
